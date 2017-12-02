c55bd47891be5b4029809ec8fffb6486037ccae7
Sharonsyra <sharonwaithii@gmail.com>
2017-12-03 00:30:28 +0300

    feature: Add api functionality/serializer

--- snippets/views.py
+++ snippets/views.py
@@ -7,3 +7,41 @@ from snippets.serializers import SnippetSerializer
 
 
 @csrf_exempt
+def snippet_list(request):
+    """ List and create code snippets """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request):
+    """ Retrieve, update or delete a code snippet """
+    try: 
+        snippet = Snippet.objects.get(pk=pk)
+    except: 
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
